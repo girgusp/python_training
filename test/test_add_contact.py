c9a3dff6ffe608d5b9ecef7d741a1097136b2f4c
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-05-22 21:56:15 +0200

    próba dojścia do tego co jest nie tak w task 5

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.contact import Contact
-from fixture.application_contact import Application
+from fixture.application import Application
 
 
 @pytest.fixture

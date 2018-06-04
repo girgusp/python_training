cb05068f996363eaa783abe096fafa63c05b4000
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-06-04 16:06:12 +0200

    optimization of switching between pages

--- fixture/contact.py
+++ fixture/contact.py
@@ -94,8 +94,10 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/addressbook")
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
+    # wd.find_element_by_link_text("home").click()
     def count(self):
         wd = self.app.wd
         # self.open_home_page()

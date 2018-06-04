c2deb83af5bc8c872e3170e6362ec7ccbc86c787
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-06-04 15:21:34 +0200

    dodanie sprawdzenia warunków wstępnych usuwania i modyfikacji grup i kontaktów

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,5 +3,10 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(first_name="test_first_name"))
+        app.contact.create(Contact(first_name="test_first_name", middle_name="test_middle_name",
+                                   last_name="test_last_name", nickname="test_nickname", title="test_title",
+                                   company="test_company", address="test_address", home="test_home",
+                                   mobile="test_mobile", work="test_work", fax="test_fax", mail1="test_mail1",
+                                   mail2="test_mail2", mail3="test_mail2", homepage="test_homepage",
+                                   address2="test_address", home2="test_home2", notes="test_notes"))
     app.contact.delete_first_contact()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,2 +1,12 @@
+from model.contact import Contact
+
+
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test_first_name", middle_name="test_middle_name",
+                                   last_name="test_last_name", nickname="test_nickname", title="test_title",
+                                   company="test_company", address="test_address", home="test_home",
+                                   mobile="test_mobile", work="test_work", fax="test_fax", mail1="test_mail1",
+                                   mail2="test_mail2", mail3="test_mail2", homepage="test_homepage",
+                                   address2="test_address", home2="test_home2", notes="test_notes"))
     app.contact.edit_first_contact()

af8cda3d993c0d0566823fa601565d411c297e3c
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-06-23 08:14:15 +0200

    cashowanie dodane dla ładowania grup list i kontaktów (kontakty do testów, po fixie zadania 11) 2. to jest commit z zadaniem 12

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,6 +12,7 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+
 # def test_add_empty_group(app):
 #    old_groups = app.group.get_group_list()
 #    group = Group(name="", header="", footer="")

1ed78a2e6bca8e3427ea88decbd05205f490dbd8
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-06-24 07:17:08 +0200

    po filmie 5.6

--- model/contact.py
+++ model/contact.py
@@ -3,9 +3,10 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, first_name=None, middle_name=None, last_name=None, nickname=None, title=None, company=None,
+    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None, nickname=None, title=None, company=None,
                  address=None, home=None, mobile=None, work=None, fax=None,
-                 mail1=None, mail2=None, mail3=None, homepage=None, address2=None, home2=None, notes=None, id=None):
+                 mail1=None, mail2=None, mail3=None, homepage=None, address2=None, home2=None, notes=None,
+                 all_phones_from_home_page=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
@@ -24,6 +25,7 @@ class Contact:
         self.address2 = address2
         self.home2 = home2
         self.notes = notes
+        self.all_phones_from_home_page = all_phones_from_home_page
         self.id = id
 
     def __repr__(self):
